c175002685e2d948560409356baac44b060d4977
Khairol Anwar <whyuwanttoknowm@gmail.com>
2023-10-25 14:56:07 +0800

    Basic functionality fully implemented

--- MemberShipFeeCalculator/MemberShipFeeCalculator/Form1.Designer.vb
+++ MemberShipFeeCalculator/MemberShipFeeCalculator/Form1.Designer.vb
@@ -22,10 +22,287 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.radSenior = New System.Windows.Forms.RadioButton()
+        Me.radStudent = New System.Windows.Forms.RadioButton()
+        Me.radChild = New System.Windows.Forms.RadioButton()
+        Me.radAdult = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.chkTrainer = New System.Windows.Forms.CheckBox()
+        Me.chkKarate = New System.Windows.Forms.CheckBox()
+        Me.chkYoga = New System.Windows.Forms.CheckBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtMonths = New System.Windows.Forms.TextBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.lblTotalFee = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblMonthlyFee = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.radSenior)
+        Me.GroupBox1.Controls.Add(Me.radStudent)
+        Me.GroupBox1.Controls.Add(Me.radChild)
+        Me.GroupBox1.Controls.Add(Me.radAdult)
+        Me.GroupBox1.Location = New System.Drawing.Point(97, 23)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(275, 148)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Types of Membership"
+        '
+        'radSenior
+        '
+        Me.radSenior.AutoSize = True
+        Me.radSenior.Location = New System.Drawing.Point(17, 110)
+        Me.radSenior.Name = "radSenior"
+        Me.radSenior.Size = New System.Drawing.Size(109, 20)
+        Me.radSenior.TabIndex = 3
+        Me.radSenior.TabStop = True
+        Me.radSenior.Text = "Senior Citizen"
+        Me.radSenior.UseVisualStyleBackColor = True
+        '
+        'radStudent
+        '
+        Me.radStudent.AutoSize = True
+        Me.radStudent.Location = New System.Drawing.Point(17, 84)
+        Me.radStudent.Name = "radStudent"
+        Me.radStudent.Size = New System.Drawing.Size(73, 20)
+        Me.radStudent.TabIndex = 2
+        Me.radStudent.TabStop = True
+        Me.radStudent.Text = "Student"
+        Me.radStudent.UseVisualStyleBackColor = True
+        '
+        'radChild
+        '
+        Me.radChild.AutoSize = True
+        Me.radChild.Location = New System.Drawing.Point(17, 58)
+        Me.radChild.Name = "radChild"
+        Me.radChild.Size = New System.Drawing.Size(143, 20)
+        Me.radChild.TabIndex = 1
+        Me.radChild.TabStop = True
+        Me.radChild.Text = "Child(12 and under)"
+        Me.radChild.UseVisualStyleBackColor = True
+        '
+        'radAdult
+        '
+        Me.radAdult.AutoSize = True
+        Me.radAdult.Location = New System.Drawing.Point(17, 31)
+        Me.radAdult.Name = "radAdult"
+        Me.radAdult.Size = New System.Drawing.Size(116, 20)
+        Me.radAdult.TabIndex = 0
+        Me.radAdult.TabStop = True
+        Me.radAdult.Text = "Standard Adult"
+        Me.radAdult.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.chkTrainer)
+        Me.GroupBox2.Controls.Add(Me.chkKarate)
+        Me.GroupBox2.Controls.Add(Me.chkYoga)
+        Me.GroupBox2.Location = New System.Drawing.Point(418, 23)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(275, 148)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Options"
+        '
+        'chkTrainer
+        '
+        Me.chkTrainer.AutoSize = True
+        Me.chkTrainer.Location = New System.Drawing.Point(17, 96)
+        Me.chkTrainer.Name = "chkTrainer"
+        Me.chkTrainer.Size = New System.Drawing.Size(129, 20)
+        Me.chkTrainer.TabIndex = 2
+        Me.chkTrainer.Text = "Personal Trainer"
+        Me.chkTrainer.UseVisualStyleBackColor = True
+        '
+        'chkKarate
+        '
+        Me.chkKarate.AutoSize = True
+        Me.chkKarate.Location = New System.Drawing.Point(17, 58)
+        Me.chkKarate.Name = "chkKarate"
+        Me.chkKarate.Size = New System.Drawing.Size(68, 20)
+        Me.chkKarate.TabIndex = 1
+        Me.chkKarate.Text = "Karate"
+        Me.chkKarate.UseVisualStyleBackColor = True
+        '
+        'chkYoga
+        '
+        Me.chkYoga.AutoSize = True
+        Me.chkYoga.Location = New System.Drawing.Point(17, 22)
+        Me.chkYoga.Name = "chkYoga"
+        Me.chkYoga.Size = New System.Drawing.Size(62, 20)
+        Me.chkYoga.TabIndex = 0
+        Me.chkYoga.Text = "Yoga"
+        Me.chkYoga.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.txtMonths)
+        Me.GroupBox3.Location = New System.Drawing.Point(97, 187)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(275, 148)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Membership Lenght"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(165, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter the Numbr of Months:"
+        '
+        'txtMonths
+        '
+        Me.txtMonths.Location = New System.Drawing.Point(17, 79)
+        Me.txtMonths.Name = "txtMonths"
+        Me.txtMonths.Size = New System.Drawing.Size(189, 22)
+        Me.txtMonths.TabIndex = 0
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.lblTotalFee)
+        Me.GroupBox4.Controls.Add(Me.Label2)
+        Me.GroupBox4.Controls.Add(Me.Label4)
+        Me.GroupBox4.Controls.Add(Me.lblMonthlyFee)
+        Me.GroupBox4.Location = New System.Drawing.Point(418, 187)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(275, 148)
+        Me.GroupBox4.TabIndex = 1
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Membership Fees"
+        '
+        'lblTotalFee
+        '
+        Me.lblTotalFee.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalFee.Location = New System.Drawing.Point(11, 110)
+        Me.lblTotalFee.Name = "lblTotalFee"
+        Me.lblTotalFee.Size = New System.Drawing.Size(135, 22)
+        Me.lblTotalFee.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 16)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Total:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(14, 32)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 16)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Monthly Fees:"
+        '
+        'lblMonthlyFee
+        '
+        Me.lblMonthlyFee.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMonthlyFee.Location = New System.Drawing.Point(11, 48)
+        Me.lblMonthlyFee.Name = "lblMonthlyFee"
+        Me.lblMonthlyFee.Size = New System.Drawing.Size(135, 22)
+        Me.lblMonthlyFee.TabIndex = 9
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(128, 386)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(137, 52)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(343, 386)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(137, 52)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(545, 386)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(137, 52)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblStatus.Location = New System.Drawing.Point(75, 347)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(653, 36)
+        Me.lblStatus.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Membership Fee Calculator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents chkTrainer As CheckBox
+    Friend WithEvents chkKarate As CheckBox
+    Friend WithEvents chkYoga As CheckBox
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents radSenior As RadioButton
+    Friend WithEvents radStudent As RadioButton
+    Friend WithEvents radChild As RadioButton
+    Friend WithEvents radAdult As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtMonths As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblMonthlyFee As Label
+    Friend WithEvents lblTotalFee As Label
+    Friend WithEvents Label2 As Label
 End Class
--- MemberShipFeeCalculator/MemberShipFeeCalculator/Form1.vb
+++ MemberShipFeeCalculator/MemberShipFeeCalculator/Form1.vb
@@ -1,3 +1,100 @@
 ﻿Public Class Form1
+    Private Sub radAdult_CheckedChanged(sender As Object, e As EventArgs) Handles radAdult.CheckedChanged
 
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim decBaseFee As Decimal
+        Dim decTotalFee As Decimal
+        Dim intMonths As Integer
+        Dim blnInputOk As Boolean = True
+
+        Const decAdultFee As Decimal = 40
+        Const decChildFee As Decimal = 20
+        Const decStudentFee As Decimal = 25
+        Const decSeniorFee As Decimal = 30
+
+        Const decYogaFee As Decimal = 10
+        Const decKarateFee As Decimal = 30
+        Const decTrainerFee As Decimal = 50
+
+        lblStatus.Text = String.Empty
+        If Integer.TryParse(txtMonths.Text, intMonths) = False Then
+            lblStatus.Text = " Months must be an integer"
+            blnInputOk = False
+
+        ElseIf intMonths < 1 Or intMonths > 24 Then
+        lblStatus.Text = "Months must be in the range of 1-24"
+            blnInputOk = False
+        End If
+
+        If blnInputOk = True Then
+            If radAdult.Checked = True Then
+                decBaseFee = decAdultFee
+            ElseIf radChild.Checked = True Then
+                decBaseFee = decChildFee
+            ElseIf radStudent.Checked = True Then
+                decBaseFee = decStudentFee
+            ElseIf radSenior.Checked = True Then
+                decBaseFee = decSeniorFee
+            End If
+
+            If chkYoga.Checked = True Then
+                decBaseFee += decYogaFee
+            End If
+
+            If chkKarate.Checked = True Then
+                decBaseFee += decKarateFee
+            End If
+
+            If chkTrainer.Checked = True Then
+                decBaseFee += decTrainerFee
+            End If
+
+            decTotalFee = decBaseFee * intMonths
+
+            lblMonthlyFee.Text = decBaseFee.ToString("c")
+            lblTotalFee.Text = decTotalFee.ToString("c")
+        End If
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        Select Case MsgBox("This will clear all data.Are you sure to proceed?", vbYesNo, "Clear Input")
+
+
+            Case MsgBoxResult.Yes
+
+                radAdult.Checked = True
+
+                chkKarate.Checked = False
+                chkYoga.Checked = False
+                chkTrainer.Checked = False
+
+                txtMonths.Clear()
+
+                lblMonthlyFee.Text = String.Empty
+                lblTotalFee.Text = String.Empty
+                lblStatus.Text = String.Empty
+
+                txtMonths.Focus()
+
+                MsgBox("You have clear all data", vbOKOnly, "Clear Input")
+
+        End Select
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Select Case MsgBox("You will exit the application.Are you sure to proceed?", vbYesNo, "Exit Application")
+
+
+            Case MsgBoxResult.Yes
+                Me.Close()
+                MsgBox("You have exit the application", vbOKOnly, "Exit Application")
+
+        End Select
+    End Sub
 End Class
